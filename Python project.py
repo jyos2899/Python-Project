09c49664120b11ab1fddd4d5067042de6ce1eee0
jyos2899 <60492937+jyos2899@users.noreply.github.com>
2023-04-27 16:49:25 -0700

    Update Python project.py

--- Python project.py
+++ Python project.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#On studying the pre existing research done in the field using the same dataset, it was found that the maximum accuracy achieved was 97%. With this model, the accuracy has been pushed to 97.84% which is significant considering the size of dataset we have used.
 
 Importing Libraries
 """
@@ -143,4 +144,3 @@ model.fit(X_train_pca,y_train,epochs=40,batch_size=128,validation_split=0.2)
 
 model.evaluate(X_test_pca,y_test)
 
-#On studying the pre existing research done in the field using the same dataset, it was found that the maximum accuracy achieved was 97%. With this model, the accuracy has been pushed to 97.84% which is significant considering the size of dataset we have used.
